096f53a93ef7edec6f538e890e4ec0dcfdc568ef
Jonathan Lam <jdlam420@gmail.com>
2015-09-22 15:54:01 -0400

    initial commit

---
+++ musician_app/models/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+  def initialize(options)
+    @id = option['id']
+    @name = option['name']
+    @email = option['email']
+  end
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def email
+    @email
+  end
+
+end
